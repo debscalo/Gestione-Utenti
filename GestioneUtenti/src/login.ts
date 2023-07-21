aea33fed447bfae91ab976fd365e8bc314fd9987
debscalo <debora.caloiero@gmail.com>
2023-07-21 11:59:29 +0200

    update

--- GestioneUtenti/src/login.ts
+++ GestioneUtenti/src/login.ts
@@ -1,4 +1,17 @@
 import { credenziali } from singup;
+interface Login {
+    email: string
+    password: string;
+}; 
+
+interface User {
+    name: string,
+    surname: string
+    address: string,
+    city: string,
+    state: string,
+    zip: string
+};
 
 function verificaDatiInseriti(){
     let email = document.getElementById("email") as HTMLInputElement;
@@ -11,6 +24,15 @@ function verificaDatiInseriti(){
 
 }
 
+
+
+
+
+let butttonSignIn= document.getElementById('signIn');
+if(butttonSignIn)
+butttonSignIn.addEventListener('click', function(){
+    window.location.href = 'login.html'
+});
 
 
 let login = document.getElementById("login");
